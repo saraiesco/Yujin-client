63f67ad8e225e6c0c88e3b80d47fffda88de0559
saraiesco <saraiescobar@google.com>
2023-11-27 22:15:03 -0600

    push before bed

--- src/components/Patients/Patients.js
+++ src/components/Patients/Patients.js
@@ -2,10 +2,19 @@ import './Patients.scss';
 import {Link} from 'react-router-dom';
 
 
-function Patients(){
+function Patients({user}){
     return(
         <div className='patients'>
-            <p>patients</p>
+            <h2 className='patients_title'>Patient List</h2>
+            <div>
+                <p>Name</p>
+                <p>Age</p>
+                <p>Gender</p>
+                <p>Diagnosis</p>
+                <p>Phone Number</p>
+                <p>Address</p>
+                <span class="material-symbols-outlined"> arrow_right_alt</span>
+            </div>
         </div>
     )
 }
